cd8f085e5b72b96baf4ab0bf48b9a17b1ac91bfa
asd84117 <39518847+asd84117@users.noreply.github.com>
2018-10-10 13:30:01 +0800

    asd
    測試

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody rgb;
+    float speed = 5;
+    float x;
+    float y;
+    Vector3 transformP;
+    private void Start()
+    {
+        rgb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        transformP = new Vector3(x, 0, y);
+        rgb.MovePosition(transform.position + transformP*Time.deltaTime*speed);
+        Quaternion dir = Quaternion.LookRotation(transformP);
+        transform.rotation = Quaternion.Slerp(transform.rotation, dir, speed);
+    }
+}
