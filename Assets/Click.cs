617bd82b736512caa5909372f4bf8e100a8d0496
FinalMaze <a731616501@163.com>
2018-10-31 12:25:53 +0800

    pb
    做了个模板

---
+++ Assets/Click.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : UIBase {
+
+    public void OnClick()
+    {
+        Debug.Log("点击了一下按钮");
+    }
+	// Use this for initialization
+	void Start () {
+        AddButtonListen("Button_N", OnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
