173dd810f4a7df05c2da2568d2e832c95d70b68f
asd84117 <39518847+asd84117@users.noreply.github.com>
2018-10-31 12:36:40 +0800

    asd
    同步進度 沒有更新東西

--- Assets/Click.cs
+++ Assets/Click.cs
@@ -11,7 +11,7 @@ public class Click : UIBase {
 	// Use this for initialization
 	void Start () {
         AddButtonListen("Button_N", OnClick);
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
