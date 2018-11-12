a8e975b35fb992cb1a551914df743a557f591a53
FinalMaze <a731616501@163.com>
2018-11-12 20:16:23 +0800

    pb
    添加了小地图功能

---
+++ Assets/LitMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LitMap : MonoBehaviour {
+    Vector3 pos;
+    Vector2 litMap;
+    float posX;
+    float posY;
+    RectTransform Map;
+    public Terrain plan;
+    GameObject player;
+	void Start ()
+    {
+        
+        player = GameObject.FindWithTag("Player");
+        Map = transform.parent.GetComponent<RectTransform>();
+	}
+	
+	void Update ()
+    {
+        pos = player.transform.position - plan.transform.position;
+        posX = pos.x / plan.terrainData.size.x;
+        posY = pos.z / plan.terrainData.size.z;
+        litMap.x = posX * Map.sizeDelta.x;
+        litMap.y = posY * Map.sizeDelta.y;
+        transform.localPosition = litMap;
+	}
+}
