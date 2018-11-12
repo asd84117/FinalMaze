7a5cc8b2207ef730fcb0e65f58562c1f60504f26
FinalMaze <a731616501@163.com>
2018-11-12 18:49:35 +0800

    pb
    优化了一下位置信息

--- Assets/Scripts/UI/EasyTouch/EasyTouchCtrl.cs
+++ Assets/Scripts/UI/EasyTouch/EasyTouchCtrl.cs
@@ -5,29 +5,29 @@ using UnityEngine.EventSystems;
 
 public class Easy
 {
-    float radius;
+    float tmpRadius;
     Vector2 tmpCore;
     GameObject tmpRocker;
     GameObject tmpPlayer;
-    public Easy(Vector2 core,GameObject rocker,float radiu,GameObject player)
+    public Easy(Vector2 core,GameObject rocker,float radius,GameObject player)
     {
         tmpPlayer = player;
         tmpCore = core;
         tmpRocker = rocker;
-        radius = radiu;
+        tmpRadius = radius;
     }
 
     public void OnDrag(BaseEventData eventData)
     {
         PointerEventData tmpDate = (PointerEventData)eventData;
         Vector2 tmpDistance = tmpDate.position - tmpCore;
-        if (tmpDistance.magnitude<radius)
+        if (tmpDistance.magnitude<tmpRadius)
         {
             tmpRocker.transform.position = tmpDate.position;
         }
         else
         {
-            tmpRocker.transform.position = tmpCore + tmpDistance.normalized * radius;
+            tmpRocker.transform.position = tmpCore + tmpDistance.normalized * tmpRadius;
         }
         Vector2 rocker = tmpPlayer.transform.localEulerAngles;
         rocker.y = 90 - Mathf.Atan2(tmpDistance.y, tmpDistance.x) * Mathf.Rad2Deg;
@@ -45,7 +45,7 @@ public class EasyTouchCtrl : UIBase
     {
         GameObject tmpEasyTouch = GetControl("Image_N");
         GameObject player = AIBase.Instance.GetAI("Player_A");
-        Easy easyTouch = new Easy(tmpEasyTouch.transform.position,tmpEasyTouch,100,player);
+        Easy easyTouch = new Easy(tmpEasyTouch.transform.position,tmpEasyTouch,75,player);
         AddDrag("Image_N", easyTouch.OnDrag);
         AddOnEndDrag("Image_N", easyTouch.OnEndDrag);
     }
